6276fcd92084a528403e3b773025bd6564ca28a1
Alejandro <aleplacambra@gmail.com>
2021-02-19 09:34:47 +0100

    Login with JWT

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -15,11 +15,7 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        return  array(
-            "token" => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGVzdCIsInBhc3N3b3JkIjoidGVzdCJ9.Qg_GTve2U265AJrjrALRN637ycFR8HCuy9QTyqx4_Qc",
-            "username" => $request['user']
-        );
-/*         $credentials = $request->only('user', 'password');
+        $credentials = $request->only('user', 'password');
         try {
             if (! $token = JWTAuth::attempt($credentials)) {
                 return response()->json(['error' => 'invalid_credentials'], 400);
@@ -27,34 +23,26 @@ class AuthController extends Controller
         } catch (JWTException $e) {
             return response()->json(['error' => 'could_not_create_token'], 500);
         }
-
-        return response()->json(compact('token')); */
+        $username = $request->input('user');
+        return response()->json(compact( 'token', 'username'));
     }
 
-/*     public function getAuthenticatedUser()
-        {
-            try {
-
-                if (! $user = JWTAuth::parseToken()->authenticate()) {
-                    return response()->json(['user_not_found'], 404);
-                }
-
-            } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
-
-                return response()->json(['token_expired'], $e->getStatusCode());
-
-            } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
-
-                return response()->json(['token_invalid'], $e->getStatusCode());
-
-            } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
-
-                return response()->json(['token_absent'], $e->getStatusCode());
-
+    public function getAuthenticatedUser()
+    {
+        try {
+            if (! $user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['user_not_found'], 404);
             }
+        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+            return response()->json(['token_expired'], $e->getStatusCode());
+        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+            return response()->json(['token_invalid'], $e->getStatusCode());
+        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+            return response()->json(['token_absent'], $e->getStatusCode());
+        }
 
-            return response()->json(compact('user'));
-        } */
+        return response()->json(compact('user'));
+    }
 
     public function like(Request $request)
     {
